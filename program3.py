def598ee9e99f42e9fb742e18d16637a6395058d
shahana9847 <shirinkaladi@gmail.com>
2026-01-06 06:52:11 +0530

    Delete program3.py

--- program3.py
+++
@@ -1,9 +0,0 @@
-text=input("enter text")
-word=text.split()
-word_count={}
-for w in word:
-    if w in word_count:
-        word_count[w]+=1
-    else:
-        word_count[w]=1
-print("word occurances:",word_count)
